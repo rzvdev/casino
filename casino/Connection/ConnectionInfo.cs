75a42370deb5007743de725045eea5a902e627d3
Razvi Andre <razviiandre@yahoo.ro>
2021-03-20 17:31:31 +0200

    # Version: 0.1 # Working on rider -Added the shining crown game -Fixed some bugs -Now the icons appear on the board in the correct order -Added table validation methods

---
+++ casino/Connection/ConnectionInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizzlingHot
+{
+    class ConnectionInfo
+    {
+        private static string _SERVER = "server=localhost; ";
+        private static string _DATABASE = "database=netdb; ";
+        private static string _USER = "user=root; ";
+        private static string _PWD = "pwd=";
+
+        protected ConnectionInfo()
+        {
+
+        }
+
+        public static string GetServerAddress()
+        {
+            return _SERVER;
+        }
+
+        public static string GetDatabaseName()
+        {
+            return _DATABASE;
+        }
+
+        public static string GetConnectionString()
+        {
+            return _SERVER + _DATABASE + _USER + _PWD;
+        }
+    }
+}
---
+++ casino/Forms/ShiningCrown.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace SizzlingHot
+{
+    partial class ShiningCrown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShiningCrown));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.table_layout = new System.Windows.Forms.TableLayoutPanel();
+            this.label1_3 = new System.Windows.Forms.Label();
+            this.label1_1 = new System.Windows.Forms.Label();
+            this.label1_4 = new System.Windows.Forms.Label();
+            this.label1_5 = new System.Windows.Forms.Label();
+            this.label2_1 = new System.Windows.Forms.Label();
+            this.label2_2 = new System.Windows.Forms.Label();
+            this.label2_3 = new System.Windows.Forms.Label();
+            this.label2_4 = new System.Windows.Forms.Label();
+            this.label2_5 = new System.Windows.Forms.Label();
+            this.label3_1 = new System.Windows.Forms.Label();
+            this.label3_2 = new System.Windows.Forms.Label();
+            this.label3_3 = new System.Windows.Forms.Label();
+            this.label3_4 = new System.Windows.Forms.Label();
+            this.label1_2 = new System.Windows.Forms.Label();
+            this.label3_5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.table_layout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.table_layout);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.Location = new System.Drawing.Point(63, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1076, 482);
+            this.panel1.TabIndex = 0;
+            // 
+            // table_layout
+            // 
+            this.table_layout.BackColor = System.Drawing.Color.White;
+            this.table_layout.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.OutsetPartial;
+            this.table_layout.ColumnCount = 5;
+            this.table_layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table_layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table_layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table_layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table_layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table_layout.Controls.Add(this.label1_1, 0, 0);
+            this.table_layout.Controls.Add(this.label1_2, 1, 0);
+            this.table_layout.Controls.Add(this.label1_3, 2, 0);
+            this.table_layout.Controls.Add(this.label1_4, 3, 0);
+            this.table_layout.Controls.Add(this.label1_5, 4, 0);
+            this.table_layout.Controls.Add(this.label2_1, 0, 1);
+            this.table_layout.Controls.Add(this.label2_2, 1, 1);
+            this.table_layout.Controls.Add(this.label2_3, 2, 1);
+            this.table_layout.Controls.Add(this.label2_4, 3, 1);
+            this.table_layout.Controls.Add(this.label2_5, 4, 1);
+            this.table_layout.Controls.Add(this.label3_1, 0, 2);
+            this.table_layout.Controls.Add(this.label3_2, 1, 2);
+            this.table_layout.Controls.Add(this.label3_3, 2, 2);
+            this.table_layout.Controls.Add(this.label3_4, 3, 2);
+            this.table_layout.Controls.Add(this.label3_5, 4, 2);
+            this.table_layout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table_layout.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.table_layout.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.table_layout.Location = new System.Drawing.Point(0, 0);
+            this.table_layout.Name = "table_layout";
+            this.table_layout.RowCount = 3;
+            this.table_layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.table_layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.table_layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.table_layout.Size = new System.Drawing.Size(1076, 482);
+            this.table_layout.TabIndex = 0;
+            // 
+            // label1_3
+            // 
+            this.label1_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1_3.Location = new System.Drawing.Point(434, 3);
+            this.label1_3.Name = "label1_3";
+            this.label1_3.Size = new System.Drawing.Size(202, 154);
+            this.label1_3.TabIndex = 2;
+            // 
+            // label1_1
+            // 
+            this.label1_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1_1.Location = new System.Drawing.Point(6, 3);
+            this.label1_1.Name = "label1_1";
+            this.label1_1.Size = new System.Drawing.Size(205, 154);
+            this.label1_1.TabIndex = 0;
+            // 
+            // label1_2
+            // 
+            this.label1_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1_2.Location = new System.Drawing.Point(220, 3);
+            this.label1_2.Name = "label1_2";
+            this.label1_2.Size = new System.Drawing.Size(202, 154);
+            this.label1_2.TabIndex = 1;
+            // 
+            // label1_4
+            // 
+            this.label1_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1_4.Location = new System.Drawing.Point(648, 3);
+            this.label1_4.Name = "label1_4";
+            this.label1_4.Size = new System.Drawing.Size(202, 154);
+            this.label1_4.TabIndex = 3;
+            // 
+            // label1_5
+            // 
+            this.label1_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1_5.Location = new System.Drawing.Point(862, 3);
+            this.label1_5.Name = "label1_5";
+            this.label1_5.Size = new System.Drawing.Size(208, 154);
+            this.label1_5.TabIndex = 4;
+            // 
+            // label2_1
+            // 
+            this.label2_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2_1.Location = new System.Drawing.Point(6, 162);
+            this.label2_1.Name = "label2_1";
+            this.label2_1.Size = new System.Drawing.Size(202, 154);
+            this.label2_1.TabIndex = 5;
+            // 
+            // label2_2
+            // 
+            this.label2_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2_2.Location = new System.Drawing.Point(220, 162);
+            this.label2_2.Name = "label2_2";
+            this.label2_2.Size = new System.Drawing.Size(202, 154);
+            this.label2_2.TabIndex = 6;
+            // 
+            // label2_3
+            // 
+            this.label2_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2_3.Location = new System.Drawing.Point(434, 162);
+            this.label2_3.Name = "label2_3";
+            this.label2_3.Size = new System.Drawing.Size(202, 154);
+            this.label2_3.TabIndex = 7;
+            // 
+            // label2_4
+            // 
+            this.label2_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2_4.Location = new System.Drawing.Point(648, 162);
+            this.label2_4.Name = "label2_4";
+            this.label2_4.Size = new System.Drawing.Size(202, 154);
+            this.label2_4.TabIndex = 8;
+            // 
+            // label2_5
+            // 
+            this.label2_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2_5.Location = new System.Drawing.Point(862, 162);
+            this.label2_5.Name = "label2_5";
+            this.label2_5.Size = new System.Drawing.Size(208, 154);
+            this.label2_5.TabIndex = 9;
+            // 
+            // label3_1
+            // 
+            this.label3_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3_1.Location = new System.Drawing.Point(6, 321);
+            this.label3_1.Name = "label3_1";
+            this.label3_1.Size = new System.Drawing.Size(202, 155);
+            this.label3_1.TabIndex = 10;
+            // 
+            // label3_2
+            // 
+            this.label3_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3_2.Location = new System.Drawing.Point(220, 321);
+            this.label3_2.Name = "label3_2";
+            this.label3_2.Size = new System.Drawing.Size(202, 155);
+            this.label3_2.TabIndex = 11;
+            // 
+            // label3_3
+            // 
+            this.label3_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3_3.Location = new System.Drawing.Point(434, 321);
+            this.label3_3.Name = "label3_3";
+            this.label3_3.Size = new System.Drawing.Size(202, 155);
+            this.label3_3.TabIndex = 12;
+            // 
+            // label3_4
+            // 
+            this.label3_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3_4.Location = new System.Drawing.Point(648, 321);
+            this.label3_4.Name = "label3_4";
+            this.label3_4.Size = new System.Drawing.Size(202, 155);
+            this.label3_4.TabIndex = 13;
+            // 
+            // label3_5
+            // 
+            this.label3_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3_5.Location = new System.Drawing.Point(862, 321);
+            this.label3_5.Name = "label3_5";
+            this.label3_5.Size = new System.Drawing.Size(205, 155);
+            this.label3_5.TabIndex = 14;
+            this.label3_5.Click += new System.EventHandler(this.label3_5_Click);
+            // 
+            // ShiningCrown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 695);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Name = "ShiningCrown";
+            this.Text = "Shining Crown";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ShiningCrown_KeyPress);
+            this.panel1.ResumeLayout(false);
+            this.table_layout.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel table_layout;
+        private System.Windows.Forms.Label label1_1;
+        private System.Windows.Forms.Label label1_2;
+        private System.Windows.Forms.Label label1_3;
+        private System.Windows.Forms.Label label1_4;
+        private System.Windows.Forms.Label label1_5;
+        private System.Windows.Forms.Label label2_1;
+        private System.Windows.Forms.Label label2_2;
+        private System.Windows.Forms.Label label2_3;
+        private System.Windows.Forms.Label label2_4;
+        private System.Windows.Forms.Label label2_5;
+        private System.Windows.Forms.Label label3_1;
+        private System.Windows.Forms.Label label3_2;
+        private System.Windows.Forms.Label label3_3;
+        private System.Windows.Forms.Label label3_4;
+        private System.Windows.Forms.Label label3_5;
+    }
+}
